4e1dc27142badadeb73d7a6bc295b86c306405c4
王跃 <wangyue57@meituan.com>
2018-05-13 17:39:40 +0800

    do not transform img to dataurl

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -8,8 +8,6 @@ function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-
-
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
@@ -45,7 +43,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 1,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
@@ -53,7 +51,7 @@ module.exports = {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 1,
           name: utils.assetsPath('media/[name].[hash:7].[ext]')
         }
       },
@@ -61,7 +59,7 @@ module.exports = {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 1,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
