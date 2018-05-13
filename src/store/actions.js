2996c6c6a373fedb02b6ab036d49f97a5edd4dde
王跃 <wangyue57@meituan.com>
2018-05-13 17:24:24 +0800

    init project

---
+++ src/store/actions.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const http = axios.create({
+  baseURL: '/vue-base',
+})
+http.interceptors.response.use(
+  response => response,
+  ({config, response}) => {
+    alert(`系统错误：${config.url.split('/').pop()} ${response.status} ${response.statusText}`)
+  }
+)
+
+export default {
+  anAction ({state, commit}, {data, callback}) {
+    http.post('/url', {...data, id: state.someState.id,}).then(({data}) => {
+      if (data.success) {
+        commit('setState', {someState: data.someState})
+        callback && callback()
+      } else {
+        alert(data.message)
+      }
+    })
+  },
+}
